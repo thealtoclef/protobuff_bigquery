9c93dab01f340c7702c8bbb47e2f5c23053df490
buu.nguyen <buu.nguyen@cake.vn>
2024-11-08 19:15:49 +0700

    Update

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Protobuf to Terraform google_bigquery_table resource
 ## Pre-requisites
 - https://grpc.io/docs/protoc-installation/
-- https://github.com/GoogleCloudPlatform/protoc-gen-bq-schema
+- https://github.com/GoogleCloudPlatform/protoc-gen-bq-schema/releases/
+
+```
+brew install go protobuf
+go install github.com/GoogleCloudPlatform/protoc-gen-bq-schema@latest
+```
--- protobuf_to_bq_schema.py
+++ protobuf_to_bq_schema.py
@@ -50,43 +50,31 @@ def generate_bigquery_schema(topic_schema_definition: str) -> str:
         ]
     )
 
-    with TemporaryDirectory() as temp_dir:
-        temp_dir = Path(temp_dir)
+    # Write the pubsub.proto file
+    with open("target/pubsub.proto", "w") as file:
+        file.write(Generator().generate(proto_schema))
 
-        # Get a list of all .proto files in the protos directory
-        proto_files = glob.glob(str(CWD / "protos" / "*.proto"))
+    # Generate the pubsub.schema file
+    subprocess.run(
+        [
+            "protoc",
+            "--bq-schema_out=target",
+            "--bq-schema_opt=single-message",
+            "target/pubsub.proto",
+        ],
+    )
 
-        # Create a symlink for each .proto file
-        for proto_file in proto_files:
-            file_name = os.path.basename(proto_file)
-            os.symlink(proto_file, temp_dir / file_name)
+    bq_schema = json.load(open("target/pubsub/pubsub.schema"))
+    bq_schema.extend(
+        [
+            {"name": "subscription_name", "type": "STRING", "mode": "NULLABLE"},
+            {"name": "message_id", "type": "STRING", "mode": "NULLABLE"},
+            {"name": "publish_time", "type": "TIMESTAMP", "mode": "NULLABLE"},
+            {"name": "attributes", "type": "JSON", "mode": "NULLABLE"},
+        ]
+    )
 
-        # Write the pubsub.proto file
-        with open(temp_dir / "pubsub.proto", "w") as file:
-            file.write(Generator().generate(proto_schema))
-
-        # Generate the pubsub.schema file
-        subprocess.run(
-            [
-                "protoc",
-                f"--bq-schema_out=.",
-                "--bq-schema_opt=single-message",
-                f"pubsub.proto",
-            ],
-            cwd=temp_dir,
-        )
-
-        bq_schema = json.load(open(temp_dir / "pubsub" / "pubsub.schema"))
-        bq_schema.extend(
-            [
-                {"name": "subscription_name", "type": "STRING", "mode": "NULLABLE"},
-                {"name": "message_id", "type": "STRING", "mode": "NULLABLE"},
-                {"name": "publish_time", "type": "TIMESTAMP", "mode": "NULLABLE"},
-                {"name": "attributes", "type": "JSON", "mode": "NULLABLE"},
-            ]
-        )
-
-        return json.dumps(bq_schema, indent=2)
+    return json.dumps(bq_schema, indent=2)
 
 
 def generate_template_variables(
@@ -120,7 +108,7 @@ def render_google_bigquery_dataset(
 ) -> str:
     # Render the Terraform template
     environment = jinja2.Environment(loader=jinja2.FileSystemLoader(f"{CWD}/templates"))
-    template = environment.get_template("google_bigquery_dataset.tf.jinja")
+    template = environment.get_template("google_bigquery_dataset.yaml.jinja")
     content = template.render(
         project_id=template_variables["project_id"],
         dataset_id=template_variables["dataset_id"],
@@ -134,7 +122,7 @@ def render_google_bigquery_table(
 ) -> str:
     # Render the Terraform template
     environment = jinja2.Environment(loader=jinja2.FileSystemLoader(f"{CWD}/templates"))
-    template = environment.get_template("google_bigquery_table.tf.jinja")
+    template = environment.get_template("google_bigquery_table.yaml.jinja")
     content = template.render(
         project_id=template_variables["project_id"],
         dataset_id=template_variables["dataset_id"],
@@ -149,16 +137,15 @@ def render_google_bigquery_table(
 
 # Example usage
 if __name__ == "__main__":
-    bigquery_table = "bef-cake-sandbox.stage_cake_recaptcha.recaptcha_message"
-    topic_schema_definition = 'syntax = "proto3";\nmessage Recaptcha {\nstring id = 1;\nstring platform = 2;\nstring user_action = 3;\nstring token_action = 4;\nfloat score = 5;\nstring client_id = 6;\nstring device_id = 7;\nstring phone = 8;\nstring risk_analysis_reasons = 9; //JSON \nstring token_properties_invalid_reason = 10;\nbool token_properties_valid = 11;\nstring additional = 12; //JSON \nstring created_at = 13; //TIMESTAMP \n}'
+    bigquery_table = "northstar-as-se1-dev.mqtt.event_fcm"
+    topic_schema_definition = "syntax = \"proto2\";\nmessage EventTracking {\nrequired string event_name = 1;\noptional string params = 2;// JSON\nrequired string event_id = 3;\noptional string client_id = 4;\nrequired string created_at = 5;// TIMESTAMP\nmap<string, string> map_params = 6;}"
     template_vars = generate_template_variables(
         resource_id=bigquery_table,
         protobuf_schema=topic_schema_definition,
         partitioning_field="created_at",
-        clustering_fields=["platform", "client_id", "device_id", "phone"],
     )
 
-    with open(CWD / "test" / "dataset.tf", "w") as f:
+    with open("target/dataset.yaml", "w") as f:
         f.write(render_google_bigquery_dataset(template_vars))
-    with open(CWD / "test" / "table.tf", "w") as f:
+    with open("target/table.yaml", "w") as f:
         f.write(render_google_bigquery_table(template_vars))
